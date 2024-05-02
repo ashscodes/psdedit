bc5da57b84449b59e09ccda7b4527702584ec508
ashscodes <70372500+ashscodes@users.noreply.github.com>
2024-05-02 15:56:54 +0100

    Initial commit

--- src/PsdEdit.Sample/Program.cs
+++ src/PsdEdit.Sample/Program.cs
@@ -9,18 +9,6 @@ internal class Program
         var file = new FileInfo(args[0]);
         var result = file.ReadPsdFile();
         var output = result.Success ? $"Read {result.Count} items." : $"Failed {result} task.";
-        // Console.WriteLine(output);
-
-        if (result.Items[7] is PsdMap psdMap)
-        {
-            if (psdMap[1] is not null && psdMap[1] is PsdComment psdComment)
-            {
-                if (psdComment.TryGetMapEntry(out PsdMapEntry mapEntry, out IPsdObject other))
-                {
-                    var lol = mapEntry;
-                    Console.WriteLine(lol.GetValue());
-                }
-            }
-        }
+        Console.WriteLine(output);
     }
 }
